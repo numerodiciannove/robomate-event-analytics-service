f66b592ad8d83145eb42888422d69bfb5124462a
numerodiciannove <roman.muraviov@cometspace.net>
2025-10-24 21:52:42 +0300

    [save]

--- app/api/urls_analytics.py
+++ app/api/urls_analytics.py
@@ -1,3 +1,5 @@
+import time
+
 from fastapi import APIRouter, status
 from pydantic import conlist
 from app.schemas.events import EventSchema
@@ -8,15 +10,20 @@ analytics_router = APIRouter()
 @analytics_router.post(
     "/events",
     status_code=status.HTTP_202_ACCEPTED,
-    # Rate Limiting
-    # dependencies=[Depends(rate_limit_dependency)]
 )
 async def ingest_events(
     events: conlist(EventSchema, min_length=1),
 ):
-
     """Accepts a JSON array of events and triggers their asynchronous processing."""
+
+    start_time = time.perf_counter()
 
     rows_processed = await event_processor.process_events(events=events)
 
-    return {"message": f"Successfully processed events.", "attempted_count": rows_processed}
+    elapsed = time.perf_counter() - start_time
+
+    return {
+        "message": "Successfully processed events.",
+        "attempted_count": rows_processed,
+        "response_time_sec": round(elapsed, 4)
+    }
